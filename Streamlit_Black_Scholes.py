18fe7437adf2ce07d55b7dd917858058598b41b8
Kheelan <75388406+MrChicken90@users.noreply.github.com>
2025-07-24 16:42:54 +0100

    Update Streamlit_Black_Scholes.py

--- Streamlit_Black_Scholes.py
+++ Streamlit_Black_Scholes.py
@@ -64,9 +64,9 @@ def black_scholes_with_greeks(S, K, T, r, sigma, option_type):
 
 # --- Sidebar for User Inputs ---
 st.sidebar.header("Black-Scholes Model")
-    st.write("`Created by:`")
-    linkedin_url = "https://www.linkedin.com/in/kheelan-sarathee-319877261/"
-    st.markdown(f'<a href="{linkedin_url}" target="_blank" style="text-decoration: none; color: inherit;"><img src="https://cdn-icons-png.flaticon.com/512/174/174857.png" width="25" height="25" style="vertical-align: middle; margin-right: 10px;">`Kheelan Sarathee`</a>', unsafe_allow_html=True)
+st.write("`Created by:`")
+linkedin_url = "https://www.linkedin.com/in/kheelan-sarathee-319877261/"
+st.markdown(f'<a href="{linkedin_url}" target="_blank" style="text-decoration: none; color: inherit;"><img src="https://cdn-icons-png.flaticon.com/512/174/174857.png" width="25" height="25" style="vertical-align: middle; margin-right: 10px;">`Kheelan Sarathee`</a>', unsafe_allow_html=True)
 
 st.sidebar.markdown("---")
 
